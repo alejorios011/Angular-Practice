403ab1452e1141c8f35e648e1ed773b238664e04
Alejandro Rios <alejandrorios2012@gmail.com>
2023-07-27 17:26:32 -0500

    Se modifica el modelo de la entidad, de una interfaz por una clase con su propio constructor

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,7 +1,15 @@
-export interface Product {
+export class Product {
     id: number;
     title: string;
     price: number;
     description: string;
     images: string[];
+
+    constructor(){
+        this.id = Math.floor(Math.random() * 100);
+        this.title = "";
+        this.price = 0;
+        this.description = "";
+        this.images = [];
+    }
 }
